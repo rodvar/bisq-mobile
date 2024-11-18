873ec759d8ca1fffc25451688967383371dc7b6c
HenrikJannsen <boilingfrog@gmx.com>
2024-11-18 17:55:05 +0700

    Remove redundant config for UserProfileRepository

--- bisqapps/androidClient/src/androidMain/kotlin/network/bisq/mobile/client/di/AndroidClientModule.kt
+++ bisqapps/androidClient/src/androidMain/kotlin/network/bisq/mobile/client/di/AndroidClientModule.kt
@@ -4,7 +4,6 @@ import network.bisq.mobile.client.service.ApiRequestService
 import network.bisq.mobile.domain.client.main.user_profile.ClientUserProfileModel
 import network.bisq.mobile.domain.client.main.user_profile.ClientUserProfileServiceFacade
 import network.bisq.mobile.domain.client.main.user_profile.UserProfileApiGateway
-import network.bisq.mobile.domain.data.repository.UserProfileRepository
 import network.bisq.mobile.domain.user_profile.UserProfileModel
 import network.bisq.mobile.domain.user_profile.UserProfileServiceFacade
 import org.koin.dsl.module
@@ -14,6 +13,5 @@ val androidClientModule = module {
     single { ApiRequestService("10.0.2.2") }
     single { UserProfileApiGateway(get()) }
     single<UserProfileServiceFacade> { ClientUserProfileServiceFacade(get(), get()) }
-    single { UserProfileRepository(get(), get()) }
 
 }
