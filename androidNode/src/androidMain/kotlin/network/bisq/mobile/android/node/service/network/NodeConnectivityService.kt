cc0b23918b28975b8b0c51c2a123764afd23626e
rodvar <1040902+rodvar@users.noreply.github.com>
2025-02-11 09:18:33 +1100

    - leaving node impl for next PR

--- androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/service/network/NodeConnectivityService.kt
+++ androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/service/network/NodeConnectivityService.kt
@@ -5,12 +5,14 @@ import network.bisq.mobile.domain.service.network.ConnectivityService
 
 class NodeConnectivityService(private val applicationService: AndroidApplicationService.Provider): ConnectivityService() {
     override fun isConnected(): Boolean {
-        return try {
-            log.d { "Connected nodes = ${applicationService.networkService.get().numberOfConnectedNodes}"}
-            applicationService.networkService.get().numberOfConnectedNodes > 0
-        } catch (e: Exception) {
-            log.w(e) { "Failed to get number of connected peers through data network service" }
-            false
-        }
+        // TODO implement this and also the ConnectivityService#newRequestRoundTripTime() call needs to be applied when a P2P roundtrip call is done
+        return true
+//        return try {
+//            log.d { "Connected nodes = ${applicationService.networkService.get().numberOfConnectedNodes}"}
+//            applicationService.networkService.get().numberOfConnectedNodes > 0
+//        } catch (e: Exception) {
+//            log.w(e) { "Failed to get number of connected peers through data network service" }
+//            false
+//        }
     }
 }
