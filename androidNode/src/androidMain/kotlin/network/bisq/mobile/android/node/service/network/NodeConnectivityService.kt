f09e2a289e91da4c851f81e003507adedaf72063
rodvar <1040902+rodvar@users.noreply.github.com>
2025-03-05 12:42:20 +1100

    - much simpler implementation based on service nodes and peer manager

--- androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/service/network/NodeConnectivityService.kt
+++ androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/service/network/NodeConnectivityService.kt
@@ -1,10 +1,5 @@
 package network.bisq.mobile.android.node.service.network
 
-import bisq.network.identity.NetworkId
-import bisq.network.p2p.message.EnvelopePayloadMessage
-import bisq.network.p2p.node.CloseReason
-import bisq.network.p2p.node.Connection
-import bisq.network.p2p.node.Node
 import network.bisq.mobile.android.node.AndroidApplicationService
 import network.bisq.mobile.domain.service.network.ConnectivityService
 
@@ -14,51 +9,8 @@ class NodeConnectivityService(private val applicationService: AndroidApplication
         const val SLOW_PEER_QUANTITY_THRESHOLD = 2
     }
 
-    private var connections = 0
-    private val nodeListener : Node.Listener = object: Node.Listener {
-            override fun onMessage(epm: EnvelopePayloadMessage?, conn: Connection?, network: NetworkId?) {
-                // do nth
-            }
-
-            override fun onConnection(connection: Connection?) {
-                log.d { "connection stablished $connection" }
-                connections++
-            }
-
-            override fun onDisconnect(connection: Connection?, closeReason: CloseReason?) {
-                if (connections > 0) {
-                    log.d { "connection lost $connection for $closeReason" }
-                    connections--
-                }
-            }
-
-        }
-
-    override fun onStart() {
-        try {
-            val networkService = applicationService.networkService.get()
-            networkService.supportedTransportTypes.forEach { transportType ->
-                val serviceNode = networkService.findServiceNode(transportType).get()
-                serviceNode.nodesById.addNodeListener(nodeListener)
-            }
-        } catch (e: Exception) {
-            log.w(e) { "failed to start node monitoring for connectivity service" }
-        }
-    }
-
-    override fun onStop() {
-        try {
-            val networkService = applicationService.networkService.get()
-            networkService.supportedTransportTypes.forEach { transportType ->
-                val serviceNode = networkService.findServiceNode(transportType).get()
-                serviceNode.nodesById.removeNodeListener(nodeListener)
-            }
-        } catch (e: Exception) {
-            log.w(e) { "failed to start node monitoring for connectivity service" }
-        }
-    }
-
     override fun isConnected(): Boolean {
+        val connections = currentConnections()
         log.d { "Connected peers = $connections"}
         return connections > 0
     }
@@ -66,6 +18,14 @@ class NodeConnectivityService(private val applicationService: AndroidApplication
     override suspend fun isSlow(): Boolean {
         // TODO improve impl using ConnectivityService#newRequestRoundTripTime() call needs to be applied when a P2P roundtrip call is done for the
         // parent isSlow impl to work
-        return connections < SLOW_PEER_QUANTITY_THRESHOLD
+        return currentConnections() < SLOW_PEER_QUANTITY_THRESHOLD
+    }
+
+    private fun currentConnections(): Int {
+        var connections = 0
+        applicationService.findAllServiceNodes().forEach {
+            connections += it.peerGroupManager.get().node.numConnections
+        }
+        return connections
     }
 }
