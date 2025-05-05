7f0c21d3f097c8296b6b1c7b847db5960ca9c4f6
rodvar <1040902+rodvar@users.noreply.github.com>
2025-05-05 16:42:53 +1000

    - i18n for node app facade

--- androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/service/bootstrap/NodeApplicationBootstrapFacade.kt
+++ androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/service/bootstrap/NodeApplicationBootstrapFacade.kt
@@ -5,6 +5,7 @@ import bisq.common.observable.Observable
 import bisq.common.observable.Pin
 import network.bisq.mobile.android.node.AndroidApplicationService
 import network.bisq.mobile.domain.service.bootstrap.ApplicationBootstrapFacade
+import network.bisq.mobile.i18n.i18n
 
 class NodeApplicationBootstrapFacade(
     private val applicationService: AndroidApplicationService.Provider
@@ -22,12 +23,12 @@ class NodeApplicationBootstrapFacade(
         applicationServiceStatePin = applicationServiceState.addObserver { state: State ->
             when (state) {
                 State.INITIALIZE_APP -> {
-                    setState("Starting Bisq")
+                    setState("splash.applicationServiceState.INITIALIZE_APP".i18n())
                     setProgress(0f)
                 }
 
                 State.INITIALIZE_NETWORK -> {
-                    setState("Initialize P2P network")
+                    setState("splash.applicationServiceState.INITIALIZE_NETWORK".i18n())
                     setProgress(0.5f)
                 }
 
@@ -36,17 +37,17 @@ class NodeApplicationBootstrapFacade(
                 }
 
                 State.INITIALIZE_SERVICES -> {
-                    setState("Initialize services")
+                    setState("splash.applicationServiceState.INITIALIZE_SERVICES".i18n())
                     setProgress(0.75f)
                 }
 
                 State.APP_INITIALIZED -> {
-                    setState("Bisq started")
+                    setState("splash.applicationServiceState.APP_INITIALIZED".i18n())
                     setProgress(1f)
                 }
 
                 State.FAILED -> {
-                    setState("Startup failed")
+                    setState("splash.applicationServiceState.FAILED".i18n())
                     setProgress(0f)
                 }
             }
