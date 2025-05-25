76bf7b2b222c118a41ca7ee8802440367472e7a6
rodvar <1040902+rodvar@users.noreply.github.com>
2025-05-25 11:55:05 +1000

    - setup localhost facade indicating emulator or not

--- androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/MainApplication.kt
+++ androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/MainApplication.kt
@@ -6,10 +6,10 @@ import android.os.Process
 import bisq.common.facades.FacadeProvider
 import bisq.common.facades.android.AndroidGuavaFacade
 import bisq.common.facades.android.AndroidJdkFacade
-import bisq.common.network.AndroidEmulatorLocalhostFacade
 import network.bisq.mobile.android.node.di.androidNodeModule
 import network.bisq.mobile.domain.di.domainModule
 import network.bisq.mobile.domain.di.serviceModule
+import network.bisq.mobile.domain.utils.Logging
 import network.bisq.mobile.presentation.di.presentationModule
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.koin.android.ext.koin.androidContext
@@ -17,7 +17,7 @@ import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import java.security.Security
 
-class MainApplication : Application() {
+class MainApplication : Application(), Logging {
     companion object {
         private val nodeModules = listOf(domainModule, serviceModule, presentationModule, androidNodeModule)
 
@@ -38,7 +38,10 @@ class MainApplication : Application() {
     }
 
     private fun setupBisqCoreStatics() {
-        FacadeProvider.setLocalhostFacade(AndroidEmulatorLocalhostFacade())
+//        FacadeProvider.setLocalhostFacade(AndroidEmulatorLocalhostFacade())
+        val isEmulator = isEmulator()
+        FacadeProvider.setIsAndroidDevice(true)
+        FacadeProvider.setIsAndroidEmulator(isEmulator)
         FacadeProvider.setJdkFacade(AndroidJdkFacade(Process.myPid()))
         FacadeProvider.setGuavaFacade(AndroidGuavaFacade())
 
@@ -46,5 +49,17 @@ class MainApplication : Application() {
         // it and add our BC provider
         Security.removeProvider("BC")
         Security.addProvider(BouncyCastleProvider())
+        log.d { "Configured bisq2 for Android${if (isEmulator) " emulator" else ""}" }
+    }
+
+    private fun isEmulator(): Boolean {
+        return android.os.Build.FINGERPRINT.startsWith("generic")
+                || android.os.Build.FINGERPRINT.startsWith("unknown")
+                || android.os.Build.MODEL.contains("google_sdk")
+                || android.os.Build.MODEL.contains("Emulator")
+                || android.os.Build.MODEL.contains("Android SDK built for x86")
+                || android.os.Build.MANUFACTURER.contains("Genymotion")
+                || (android.os.Build.BRAND.startsWith("generic") && android.os.Build.DEVICE.startsWith("generic"))
+                || "google_sdk".equals(android.os.Build.PRODUCT);
     }
 }
