35460fdb1d08ac4e868fc9c27fbd1578b2a9057e
rodvar <1040902+rodvar@users.noreply.github.com>
2025-07-17 11:26:54 +1000

    - node network stats facade impl for published profiles

--- androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/service/network_stats/NodeNetworkStatsServiceFacade.kt
+++ androidNode/src/androidMain/kotlin/network/bisq/mobile/android/node/service/network_stats/NodeNetworkStatsServiceFacade.kt
@@ -1,5 +1,7 @@
 package network.bisq.mobile.android.node.service.network_stats
 
+import bisq.common.observable.Pin
+import kotlinx.coroutines.Job
 import network.bisq.mobile.android.node.AndroidApplicationService
 import network.bisq.mobile.domain.service.network_stats.NetworkStatsServiceFacade
 import network.bisq.mobile.domain.utils.Logging
@@ -8,21 +10,35 @@ class NodeNetworkStatsServiceFacade(
     private val applicationService: AndroidApplicationService.Provider
 ) : NetworkStatsServiceFacade(), Logging {
 
+    private var job: Job? = null
+    private var publishedProfilesCountPin: Pin? = null
+
     override fun activate() {
         super.activate()
-        
-        // TODO: Access P2P network services to get real stats
-        // val userService = applicationService.userService
-        // val networkService = applicationService.networkService
-        
-        // Mock data for now
-        _publishedProfilesCount.value = 2150
 
-        log.d { "NodeNetworkStatsServiceFacade activated with mock data" }
+        observePublishedProfilesCount()
+
+        log.d { "NodeNetworkStatsServiceFacade activated" }
     }
 
     override fun deactivate() {
+        publishedProfilesCountPin?.unbind()
+        publishedProfilesCountPin = null
         super.deactivate()
         log.d { "NodeNetworkStatsServiceFacade deactivated" }
+    }
+
+    private fun observePublishedProfilesCount() {
+        job?.cancel()
+        job = launchIO {
+            val userService = applicationService.userService
+            val userProfileService = userService.get().userProfileService
+
+            publishedProfilesCountPin?.unbind()
+            publishedProfilesCountPin = userProfileService.numUserProfiles.addObserver { num ->
+                _publishedProfilesCount.value = num
+                log.d { "Published profiles count updated: ${_publishedProfilesCount.value}" }
+            }
+        }
     }
 }
