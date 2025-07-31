40253be70672140037bb000d2e45809224f413d1
nostrbuddha <nostrbuddha@proton.me>
2025-07-31 20:40:03 +0530

    - fix: Create offer review screen: Not showing Range amount for Fiat amount

--- shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/components/molecules/AmountWithCurrency.kt
+++ shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/components/molecules/AmountWithCurrency.kt
@@ -7,22 +7,45 @@ import androidx.compose.ui.Alignment
 import network.bisq.mobile.presentation.ui.components.atoms.BisqText
 import network.bisq.mobile.presentation.ui.components.atoms.layout.BisqGap
 
+// Highly tied to how formattedPrice is formatted.
 @Composable
 fun AmountWithCurrency(
     formattedPrice: String,
 ) {
-    val priceFragments = formattedPrice.split(" ")
-        val value = priceFragments[0]
+    val isRangeAmount = formattedPrice.contains("-")
 
-    Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.Bottom,
+    if(isRangeAmount) {
+        val priceRange = formattedPrice.split("-")
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
         ) {
-            BisqText.largeRegular(text = value)
-
-            if(priceFragments.size == 2) {
-                BisqGap.HHalf()
-                BisqText.baseRegularGrey(priceFragments[1])
-            }
+            _SingleAmountWithCurrency(priceRange[0].trim())
+            BisqGap.HHalf()
+            BisqText.largeRegular(text = "-")
+            BisqGap.HHalf()
+            _SingleAmountWithCurrency(priceRange[1].trim())
         }
+    } else {
+        _SingleAmountWithCurrency(formattedPrice)
+    }
+}
+
+@Composable
+fun _SingleAmountWithCurrency(
+    formattedPrice: String,
+) {
+    val priceFragments = formattedPrice.split(" ")
+    val value = priceFragments[0]
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.Bottom,
+    ) {
+        BisqText.largeRegular(text = value)
+
+        if(priceFragments.size == 2) {
+            BisqGap.HHalf()
+            BisqText.baseRegularGrey(priceFragments[1])
+        }
+    }
 }
