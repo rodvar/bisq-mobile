9717491d9b7cc2d4ed6a8a090e88ba261afb9106
wodoro <woodencore@proton.me>
2025-09-24 06:08:33 +0330

    fix: remove stop button for now

--- shared/presentation/src/androidMain/kotlin/network/bisq/mobile/presentation/notification/ForegroundService.android.kt
+++ shared/presentation/src/androidMain/kotlin/network/bisq/mobile/presentation/notification/ForegroundService.android.kt
@@ -2,7 +2,6 @@ package network.bisq.mobile.presentation.notification
 
 import android.annotation.SuppressLint
 import android.app.Notification
-import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.content.pm.ServiceInfo
@@ -25,7 +24,6 @@ import org.koin.android.ext.android.inject
 open class ForegroundService : Service(), Logging {
     companion object {
         const val SERVICE_NOTIF_ID = 1
-        const val STOP_SERVICE_ACTION = "STOP_SERVICE_ACTION"
     }
 
     private val notificationController: NotificationControllerImpl by inject()
@@ -34,29 +32,12 @@ open class ForegroundService : Service(), Logging {
         val contentPendingIntent =
             notificationController.createNavDeepLinkPendingIntent(Routes.TabOpenTradeList)
 
-        val stopIntent = Intent(applicationContext, ForegroundService::class.java).apply {
-            action = STOP_SERVICE_ACTION
-        }
-        val stopPendingIntent = PendingIntent.getService(
-            applicationContext,
-            SERVICE_NOTIF_ID + 1,
-            stopIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        )
-
         return NotificationCompat.Builder(this, AndroidNotificationChannels.BISQ_SERVICE_CHANNEL_ID)
             .setContentTitle("mobile.bisqService.title".i18n())
             .setContentText("mobile.bisqService.subTitle".i18n())
             .setSmallIcon(ResourceUtils.getNotifResId(applicationContext))
             .setOngoing(true)
             .setContentIntent(contentPendingIntent)
-            .addAction(
-                NotificationCompat.Action(
-                    android.R.drawable.ic_menu_close_clear_cancel,
-                    "mobile.action.notifications.stop".i18n(),
-                    stopPendingIntent
-                )
-            )
             .build()
     }
 
@@ -74,12 +55,6 @@ open class ForegroundService : Service(), Logging {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        if (intent?.action == STOP_SERVICE_ACTION) {
-            ServiceCompat.stopForeground(this, ServiceCompat.STOP_FOREGROUND_REMOVE)
-            log.i { "Notification foreground service stopped by user" }
-            stopSelf()
-            return START_NOT_STICKY
-        }
         log.i { "Service starting sticky" }
         return START_STICKY
     }
